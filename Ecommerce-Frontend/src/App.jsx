a085c54cfd52c523454c8b3bedc8e3a64cbe902d
RishabJha134 <rishabjha134@gmail.com>
2024-11-02 21:13:09 +0530

    add cart page

--- Ecommerce-Frontend/src/App.jsx
+++ Ecommerce-Frontend/src/App.jsx
@@ -3,6 +3,8 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Cart from "./components/cart/Cart";
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +18,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
   },
 ]);
 
---
+++ Ecommerce-Frontend/src/pages/CartPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Cart from "../components/cart/Cart";
+
+const CartPage = () => {
+  return <Cart></Cart>;
+};
+
+export default CartPage;
